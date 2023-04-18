c61d8dc3c9135e6cba800eb5adbd2b7fc5c52878
Hailey Rae <haileyraehughes89@gmail.com>
2023-04-18 18:33:51 -0500

    first attempt to color code blocks based on past/present/future time blocks

--- Develop/script.js
+++ Develop/script.js
@@ -3,37 +3,46 @@
 // in the html.
 
 $(document).ready(function () {
-var save = $("button"); //created variable to apply to all button elements in HTML
-var today = dayjs(); // this is defining the variable today as the function linked in the html document
+var save = $("button"); 
+var today = dayjs(); 
 
-$('#currentDay').text(today.format('MMM D, YYYY')); // posts current date using the function defined in the today variable
+
+
+$('#currentDay').text(today.format('MMM D, YYYY')); 
   //getting localstorage
 //var itemKey = localStorage.getItem("hour-9");
   //console.log(itemKey)
 
 
-
-  save.on('click', function (event) {
+  save.on('click', function testing (event) {
   var userInput = $(this).siblings("textarea").val();
-  //this is setting the variable for the text users type in. The variable's name is userInput. It is everything that belongs to the save variable. In this case that is the button element and its child element i. siblings grabs associated child of the parent div. the sibling named is the textarea element. 
+ 
   var timeBlockId = $(this).parent().attr("id");
-  // sets timeBlockId to save's parent, focusing on the id attribute only
-   
-  //   // timeBlock.push(userInput)
-  localStorage.setItem(timeBlockId, userInput); //setItem adds the timeBlockID and userInput to local storage
-  //   // console.log(userInput)
+  
+  localStorage.setItem(timeBlockId, userInput);
+  var test = localStorage.getItem(timeBlockId);
 
-  localStorage.getItem(timeBlockId, userInput);
-   
+})
+
+for (var i = 0; i < localStorage.length; i++) {
+  var savedInput = localStorage.key(i);//.key returns name of key at specified index
+  var output = localStorage.getItem(savedInput);//sets variable for data at that unknown index
+  var textarea = $("#" + savedInput).find("textarea");//$ is an alias for document.queryselector, id + savedInput (the variable) .find- look in the parent to see if there is a textarea element
+textarea.val(output)
+colorCoding (textarea, savedInput);
+}
+
+function colorCoding (textarea, savedInput) {
+  var hourValue = parseInt(savedInput.split("-")[1]);
+  var hourDayJS = dayjs().hour(hourValue)
+  console.log(hourDayJS)//working here
+  
+}
+
   });
 
-
-
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  //});
+  
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -46,4 +55,3 @@ $('#currentDay').text(today.format('MMM D, YYYY')); // posts current date using 
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
