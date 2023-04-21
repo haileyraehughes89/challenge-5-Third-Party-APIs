11b043233b9d653cd1f535cf6b95e890fb5062b3
Hailey Rae <haileyraehughes89@gmail.com>
2023-04-21 12:42:07 -0500

    successful addition of color coding upon initial load

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,33 @@
+
 $(document).ready(function () {
+
 var save = $("button"); 
 var today = dayjs();
 
-$('#currentDay').text(today.format('MMM D, YYYY')); 
+
+$('#currentDay').text(today.format('MMM D, YYYY'));
+var timeBlocks = [
+  {id: '9', hour: 9},
+  {id: '10', hour: 10},
+  {id: '11', hour: 11},
+  {id: '12', hour: 12},
+  {id: '13', hour: 13},
+  {id: '14', hour: 14},
+  {id: '15', hour: 15},
+  {id: '16', hour: 16},
+  {id: '17', hour: 17},
+];
+
+timeBlocks.forEach(function(colorCoding){
+  var textarea = $("#" + colorCoding.id).find("textarea");
+  var currentTime = new Date() //date and time
+  var now = currentTime.getHours();//pulling just the hour value
+     if (now > colorCoding.id) {console.log("past"); textarea.addClass("past")
+     } else if (now == colorCoding.id) {console.log("now"); textarea.addClass("present")
+     } else {console.log("future"); textarea.addClass("future")}
+})
+
+
 
   save.on('click', function testing (event) {
   var userInput = $(this).siblings("textarea").val();
@@ -10,32 +35,32 @@ $('#currentDay').text(today.format('MMM D, YYYY'));
   var timeBlockId = $(this).parent().attr("id");
   
   localStorage.setItem(timeBlockId, userInput);
-  var test = localStorage.getItem(timeBlockId);
+  //var test = localStorage.getItem(timeBlockId);
+  })
+
+  for (var i = 0; i < 10; i++) {
+    var savedInput = localStorage.key(i);//.key returns name of key at specified index
+    var output = localStorage.getItem(savedInput);//sets variable for data at that unknown index
+    var textarea = $("#" + savedInput).find("textarea");//$ is an alias for document.queryselector, id + savedInput (the variable) .find- look in the parent to see if there is a textarea element
+  textarea.val(output)
+  //colorCoding(textarea, savedInput);
+  }
 
 })
 
-for (var i = 0; i < localStorage.length; i++) {
-  var savedInput = localStorage.key(i);//.key returns name of key at specified index
-  var output = localStorage.getItem(savedInput);//sets variable for data at that unknown index
-  var textarea = $("#" + savedInput).find("textarea");//$ is an alias for document.queryselector, id + savedInput (the variable) .find- look in the parent to see if there is a textarea element
-textarea.val(output)
-colorCoding (textarea, savedInput);
-}
 
-function colorCoding (textarea, savedInput) {
-  var hourValue = parseInt(savedInput.split("-")[1]);
-  //console.log(hourValue);
-  var currentTime = new Date()
-  var now = currentTime.getHours();
-  console.log(now);
-  var blockColor = document.querySelectorAll("textarea");
-    if (now == hourValue) {console.log("now")}
-    else {console.log("not now");}
 
-  };
-  
+// function colorCoding (textarea, savedInput) {
+//   var hourValue = parseInt(savedInput); //block time on webpage
+//   var currentTime = new Date() //date and time
+//   var now = currentTime.getHours();//pulling just the hour value
+//     if (now > hourValue) {console.log("past"); textarea.addClass("past")
+//     } else if (now == hourValue) {console.log("now"); textarea.addClass("present")
+//     } else {console.log("future"); textarea.addClass("future")}
+//   };
 
-});
+
+
   
   //
   // TODO: Add code to apply the past, present, or future class to each time
